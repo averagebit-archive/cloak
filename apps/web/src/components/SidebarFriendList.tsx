31d751e415035c1926ea39520a1bb894d10b7d07
averagebit <averagebit@pm.me>
2022-10-12 13:20:42 +0000

    fix: friend list spacing

--- apps/web/src/components/SidebarFriendList.tsx
+++ apps/web/src/components/SidebarFriendList.tsx
@@ -2,9 +2,10 @@ import SidebarFriend from "./SidebarFriend";
 
 const SidebarFriendList = () => {
     return (
-        <div class="flex flex-col h-full overflow-y-auto pt-2 px-4">
+        <div class="flex flex-col h-full space-y-1 overflow-y-auto pt-2 px-4">
             <SidebarFriend active={true} name="factor" />
             <SidebarFriend name="prime" />
+            <SidebarFriend name="some rando" />
         </div>
     );
 };
