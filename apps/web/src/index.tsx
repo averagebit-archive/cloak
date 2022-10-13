c4a66955653006e46b2a11fc1ef542e0f15c7fdb
averagebit <averagebit@pm.me>
2022-10-13 20:56:59 +0000

    feat: add solid dev tools

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,15 +1,20 @@
 /* @refresh reload */
+import { DevtoolsOverlay } from "@solid-devtools/overlay";
 import { render } from "solid-js/web";
 import { Router } from "@solidjs/router";
+import { StoreProvider } from "./store";
 
 import "./assets/styles/index.css";
 import App from "./App";
 
 render(
     () => (
-        <Router>
-            <App />
-        </Router>
+        <StoreProvider>
+            <Router>
+                <App />
+                <DevtoolsOverlay />
+            </Router>
+        </StoreProvider>
     ),
     document.getElementById("root") as HTMLElement
 );
