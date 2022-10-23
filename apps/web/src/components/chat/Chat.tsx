aba312fea4923dc1ba19a5b33a87b429127da846
Luke Babich <luke@coldbrewstudios.co.uk>
2022-10-23 19:31:39 +0100

    fix: updated chat page to use new user store

--- apps/web/src/components/chat/Chat.tsx
+++ apps/web/src/components/chat/Chat.tsx
@@ -1,11 +1,13 @@
 import ChatHeader from "./ChatHeader";
-import ChatContent from "./ChatContent";
 import ChatForm from "./ChatForm";
+import { useUserStore } from "../../store/auth";
 
 const Chat = () => {
+    const [user] = useUserStore();
+
     return (
         <div class="flex flex-col flex-auto flex-shrink-0 h-full bg-surface0">
-            <ChatHeader username="username" />
+            <ChatHeader username={user.username} />
             {/* <ChatContent messages={} /> */}
             <ChatForm />
         </div>
