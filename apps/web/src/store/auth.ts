70c88933bdb6c1edc039eff6184f6043ee199c13
Luke Babich <luke@coldbrewstudios.co.uk>
2022-10-24 19:14:34 +0100

    feature: cleaned up auth types

--- apps/web/src/store/auth.ts
+++ apps/web/src/store/auth.ts
@@ -4,49 +4,36 @@ import { createStore } from "solid-js/store";
 import { createContext, createEffect, createResource, useContext } from "solid-js";
 import { http } from "../http";
 
-export type UserStore = User & { authenticated: boolean };
-export type AuthStore = Store<UserStore> | UserStore;
-
-export type AuthActions = {
+type UserStore = User & { authenticated: boolean };
+type AuthStore = Store<UserStore> | UserStore;
+type AuthActions = {
     setUserAuthenticated: () => void;
     setUser: (user?: User | false) => void;
 };
-
-const defaultUserStore = {
-    id: Infinity,
-    username: "",
-    token: "",
-    authenticated: false
-};
+export type AuthContextValue = [AuthStore, AuthActions];
 
 export const AuthContext = createContext();
-export const useUserStore = (): any => useContext(AuthContext) as any;
+export const useUserStore = (): AuthContextValue => useContext<AuthContextValue>(AuthContext);
 
 export const createAuth = (): [AuthStore, AuthActions] => {
-    const [store, setStore] = createStore({ ...defaultUserStore }, {
+    const defaultUserStore = {
+        id: Infinity,
+        username: "",
+        token: "",
+        authenticated: false
+    };
+
+    const [store, setStore] = createStore(defaultUserStore, {
         name: "user"
     });
 
-    createEffect(() => {
-        store.token
-            ? localStorage.setItem("token", store.token)
-            : localStorage.removeItem("token");
-    });
+    createEffect(() => store.token
+        ? localStorage.setItem("token", store.token)
+        : localStorage.removeItem("token"));
 
     const actions: AuthActions = {
-        setUserAuthenticated: () => {
-            setStore({
-                authenticated: true
-            });
-        },
-        setUser: (user: User | false) => {
-            if (!user) {
-                setStore(defaultUserStore)
-                return
-            }
-
-            setStore(user);
-        },
+        setUserAuthenticated: () => setStore("authenticated", true),
+        setUser: (user: User | false) => user ? setStore(user) : setStore(defaultUserStore)
     };
 
     return [store, actions];
