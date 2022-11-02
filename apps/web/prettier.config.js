ac74d073f81712895992a234adbbaf05aee37f03
averagebit <averagebit@pm.me>
2022-11-02 12:50:39 +0000

    chore: add prettier config

---
+++ apps/web/prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    trailingComma: "es5",
+    semi: true,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    quoteProps: "consistent",
+    bracketSpacing: false,
+    bracketSameLine: false,
+    arrowParens: "always",
+    proseWrap: "preserve",
+    singleAttributePerLine: true,
+};
